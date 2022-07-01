9c19e0460179fb246248845765d61d83a2c7c355
EverSeenTOTOTO <everseenflash@163.com>
2022-07-01 18:19:41 +0800

    feat: add simple repl, fix assignment return

--- cli.js
+++ cli.js
@@ -1,20 +1,46 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
+const readline = require('readline');
 const fs = require('fs');
 const { evaluate, createGlobalEnv } = require('./dist/index.js');
 
 const files = process.argv.slice(2);
-const errors = [];
 
-for (const file of files) {
-  try {
-    const content = fs.readFileSync(file, 'utf8');
+if (files.length === 0) {
+  console.log('Welcome to [square].');
 
-    evaluate(content, createGlobalEnv(fs));
-  } catch (e) {
-    console.error(e);
-    errors.push(e instanceof Error ? e : new Error(`Read and eval ${file} failed.`));
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'sq> ',
+  });
+  const env = createGlobalEnv(fs);
+
+  rl.prompt();
+  rl.on('line', (line) => {
+    try {
+      const content = evaluate(line, env)[0];
+
+      console.log(content);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      rl.prompt();
+    }
+  });
+} else {
+  const errors = [];
+
+  for (const file of files) {
+    try {
+      const content = fs.readFileSync(file, 'utf8');
+
+      evaluate(content, createGlobalEnv(fs));
+    } catch (e) {
+      console.error(e);
+      errors.push(e instanceof Error ? e : new Error(`Read and eval ${file} failed.`));
+    }
   }
-}
 
-console.log(`\n${files.length} scripts evaluated, ${files.length - errors.length} success, ${errors.length} fail.\n`);
+  console.log(`\n${files.length} scripts evaluated, ${files.length - errors.length} success, ${errors.length} fail.\n`);
+}
